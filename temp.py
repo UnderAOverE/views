ead74a2c61c28eb89165acef3cffa0d405177bea
Shane Reddy <95642704+UnderAOverE@users.noreply.github.com>
2025-06-20 14:55:50 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,30 @@
-
+"related_items": [
+  {
+    "table": "task_ci",
+    "record_count": 122.0,
+    "remarks": [],
+    "data": [
+      {
+        "ci_item": "ABCD-PRD1924"
+      },
+      {
+        "ci_item": "EFGH-PRD3922"
+      },
+      {
+        "ci_item": "IJKL-PRD3923\\\\ACQ855GT1-SRV5"
+      },
+      {
+        "ci_item": "MNOP-PRD1902"
+      },
+      {
+        "ci_item": "QRST-PRD1922"
+      }
+    ]
+  },
+  {
+    "table": "sysapproval_group",
+    "record_count": 0.0,
+    "remarks": [],
+    "data": []
+  }
+]
